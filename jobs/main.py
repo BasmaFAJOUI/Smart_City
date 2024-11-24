65eded5dadc716692bf6610fdf879020b13ea372
BasmaFAJOUI <fajouianass@gmail.com>
2024-11-24 23:58:18 +0100

    aws added

--- jobs/main.py
+++ jobs/main.py
@@ -1,6 +1,7 @@
 import datetime
 import os
 import random
+import time
 import uuid
 from confluent_kafka import SerializingProducer
 import simplejson as json
@@ -8,12 +9,12 @@ import simplejson as json
 LONDON_COORDINATES = {"latitude": 51.5074, "longitude": -0.1278}
 BIRMINGHAM_COORDINATES = {"latitude": 52.4862, "longitude": -1.8904}
 
-# calculate mouvemlent- increment
+# calculate mouvement-increment
 
 LATITUDE_INCREMENT = (
-    BIRMINGHAM_COORDINATES['latitude']-LONDON_COORDINATES["latitude"])
+    BIRMINGHAM_COORDINATES['latitude']-LONDON_COORDINATES["latitude"])/100
 LONGITUDE_INCREMENT = (
-    BIRMINGHAM_COORDINATES['longitude']-LONDON_COORDINATES["longitude"])
+    BIRMINGHAM_COORDINATES['longitude']-LONDON_COORDINATES["longitude"])/100
 
 # environnement variables for configuration
 
@@ -68,7 +69,7 @@ def generate_vehicle_data(device_id):
 
 def generate_gps_data(device_id, timestamp, vehicle_type='private'):
     return {
-        'id': uuid.uuid4,
+        'id': uuid.uuid4(),
         'deviceId': device_id,
         'timestamp': timestamp,
         'speed': random.uniform(0, 40),
@@ -118,6 +119,31 @@ def generate_emergency_incident_data(device_id, timestamp, location):
     }
 
 
+def json_serializer(obj):
+    if isinstance(obj, uuid.UUID):
+        return str(obj)
+    raise TypeError(f'Object of type {
+                    obj.__class__.__name__} is not JSON serializable')
+
+
+def delivery_report(err, msg):
+    if err is not None:
+        print(f'Message delivery failed:{err}')
+    else:
+        print(f'Message delivered to {msg.topic()} [{msg.partition()}]')
+
+
+def produce_data_to_kafka(producer, topic, data):
+    producer.produce(
+        topic,
+        key=str(data['id']),
+        value=json.dumps(data, default=json_serializer).encode('utf-8'),
+        on_delivery=delivery_report
+
+    )
+    producer.flush()
+
+
 def simulate_journey(producer, device_id):
     while True:
         vehicle_data = generate_vehicle_data(device_id)
@@ -129,13 +155,19 @@ def simulate_journey(producer, device_id):
         emergency_incident_data = generate_emergency_incident_data(
             device_id, vehicle_data['timestamp'], vehicle_data['location'])
 
-        print(vehicle_data)
-        print(gps_data)
-        print(traffic_camera_data)
-        print(weather_data)
-        print(emergency_incident_data)
+        if (vehicle_data['location'][0] >= BIRMINGHAM_COORDINATES['latitude']
+                and vehicle_data['location'][1] <= BIRMINGHAM_COORDINATES['longitude']):
+            print('Vehicle has reached Birmingham.Simulation ending...')
+            break
 
-        break
+        produce_data_to_kafka(producer, VEHICLE_TOPIC, vehicle_data)
+        produce_data_to_kafka(producer, GPS_TOPIC, gps_data)
+        produce_data_to_kafka(producer, TRAFFIC_TOPIC, traffic_camera_data)
+        produce_data_to_kafka(producer, WEATHER_TOPIC, weather_data)
+        produce_data_to_kafka(producer, EMERGENCY_TOPIC,
+                              emergency_incident_data)
+
+        time.sleep(5)
 
 
 if __name__ == "__main__":
--- jobs/spark-city.py
+++ jobs/spark-city.py
@@ -1,1 +1,133 @@
-/jobs 
+from pyspark.sql import SparkSession
+from config import configuration
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, TimestampType, DoubleType
+from pyspark.sql.functions import from_json, col
+
+
+def main():
+    spark = SparkSession.builder.appName("SmartCityStreaming")\
+        .config("spark.jars.packages",
+                "org.apache.spark:spark-sql-kafka-0-10_2.13:3.5.0",
+                "org.apache.hadoop:hadoop-aws:3.3.1",
+                "com.amazonaws:aws-java-sdk-core:1.11.469")\
+        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")\
+        .config("spark.hadoop.fs.s3a.access.key", configuration.get('AWS_ACCESS_KEY'))\
+        .config("spark.hadoop.fs.s3a.secret.key", configuration.get('AWS_SECRET_KEY'))\
+        .config('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.impl.SimpleAWSCredentialsProvider')\
+        .getOrCreate()
+
+    # adjust the log level to minimize the console output on executors
+    spark.sparkContext.setLogLevel('WARN')
+
+    # vehicle schema
+    vehicleSchema = StructType([
+        StructField("id", StringType(), True),
+        StructField("deviceid", StringType(), True),
+        StructField("timestamp", TimestampType(), True),
+        StructField("location", StringType(), True),
+        StructField("speed", DoubleType(), True),
+        StructField("direction", StringType(), True),
+        StructField("make", StringType(), True),
+        StructField("model", StringType(), True),
+        StructField("year", IntegerType(), True),
+        StructField("fuelType", StringType(), True),
+
+
+    ])
+
+    # gpsSchema
+    gpsSchema = StructType([
+        StructField("id", StringType(), True),
+        StructField("deviceid", StringType(), True),
+        StructField("timestamp", TimestampType(), True),
+        StructField("speed", DoubleType(), True),
+        StructField("direction", StringType(), True),
+        StructField("vehicleType", StringType(), True),
+
+
+    ])
+
+    # trafficSchema
+    trafficSchema = StructType([
+        StructField("id", StringType(), True),
+        StructField("deviceid", StringType(), True),
+        StructField("cameraId", StringType(), True),
+        StructField("location", StringType(), True),
+        StructField("timestamp", TimestampType(), True),
+        StructField("snap", StringType(), True),
+
+
+    ])
+    # weatherSchema
+    weatherSchema = StructType([
+        StructField("id", StringType(), True),
+        StructField("deviceid", StringType(), True),
+        StructField("location", StringType(), True),
+        StructField("timestamp", TimestampType(), True),
+        StructField("Temperature", DoubleType(), True),
+        StructField("weatherCondition", StringType(), True),
+        StructField("precipitation", StringType(), True),
+        StructField("windSpeed", StringType(), True),
+        StructField("humidity", StringType(), True),
+        StructField("airQualityIndex", StringType(), True),
+    ])
+    # emergencySchema
+    emergencySchema = StructType([
+        StructField("id", StringType(), True),
+        StructField("deviceid", StringType(), True),
+        StructField("incidentId", StringType(), True),
+        StructField("type", StringType(), True),
+        StructField("timestamp", TimestampType(), True),
+        StructField("location  ", StringType(), True),
+        StructField("status", StringType(), True),
+        StructField("description", StringType(), True),
+    ])
+
+    def read_kafka_topic(topic, schema):
+        return (spark.readStram
+                .format('kafka')
+                .option('kafka.bootstrap.servers', 'broker:29092')
+                .option('subscribe', topic)
+                .option('startingOffsets', 'earliest')
+                .load()
+                .selectExpr('CASR(values AS STRING)')
+                .select(from_json(col('value'), schema).alias('data'))
+                .select('data.*')
+                .withWatermark('timestamp', vehicleSchema).alias('vehicle')
+                )
+
+    def streamWriter(DataFrale, checkPointFolder, output):
+        return (input.writeStream
+                .format('parquet')
+                .option('checkpointLocation', checkPointFolder)
+                .option('path', output)
+                .outputMode('append')
+                .start()
+                )
+
+    vehicleDF = read_kafka_topic(
+        'vehicle_data', vehicleSchema).alias('vehicle')
+    gpsDF = read_kafka_topic('gps_data', vehicleSchema).alias('gps')
+    trafficDF = read_kafka_topic(
+        'traffic_data', vehicleSchema).alias('traffic')
+    weatherDF = read_kafka_topic(
+        'weather_data', weatherSchema).alias('weather')
+    emergencyDF = read_kafka_topic(
+        'emergency_data', emergencySchema).alias('emergency')
+
+    # join all the dfs with
+
+    query1 = streamWriter(vehicleDF, 's3a://spark-streaming-data-basmafj/checkpoints/vehicle_data',
+                          's3a://spark-streaming-data-basmafj/vehicle_data')
+    query2 = streamWriter(gpsDF, 's3a://spark-streaming-data-basmafj/checkpoints/gps_data',
+                          's3a://spark-streaming-data-basmafj/vehicle_data')
+    query3 = streamWriter(trafficDF, 's3a://spark-streaming-data-basmafj/checkpoints/vehicle_data',
+                          's3a://spark-streaming-data-basmafj/traffic_data')
+    query4 = streamWriter(weatherDF, 's3a://spark-streaming-data-basmafj/checkpoints/vehicle_data',
+                          's3a://spark-streaming-data-basmafj/weather_data')
+    query5 = streamWriter(emergencyDF, 's3a://spark-streaming-data-basmafj/checkpoints/vehicle_data',
+                          's3a://spark-streaming-data-basmafj/emegency_data')
+
+
+if __name__ == "__main__":
+    main()
